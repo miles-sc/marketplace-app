27d4dff42f013d9834808f8bebeac27b018b2cbe
Miles Cameron <miles.cameron.nv@gmail.com>
2025-06-23 16:19:42 -0500

    add pinia material store

---
+++ frontend/src/stores/materials.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useMaterialsStore=defineStore('materials',()=> {
+
+    const materials = ref([])
+
+    async function fetchMaterials(){
+        try{
+            const response=await fetch('http://localhost:3000/products')
+            materials.value=await response.json()
+        } catch (error) {
+            console.error('Error fetching materials:',error)
+        }
+    }
+
+    return { materials, fetchMaterials}
+})
