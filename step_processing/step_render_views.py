2275274bce785a14f33889763adbf6dbdda9f4c6
Miles Cameron <miles.cameron.nv@gmail.com>
2025-08-13 22:03:16 -0500

    remove png conversion provisions

--- step_processing/step_render_views.py
+++ step_processing/step_render_views.py
@@ -5,28 +5,14 @@ import argparse
 import cadquery as cq
 from cadquery import exporters, importers
 
-# try:
-#     import cairosvg
-#     HAS_CAIROSVG = True
-# except Exception:
-#     HAS_CAIROSVG = False
-
 def export_svg(obj, out_svg: Path, projection_dir, stroke_width=0.5, show_hidden=False):
     opts = {
         "projectionDir": projection_dir,
         "showAxes": False,
         "showHidden": show_hidden,
         "strokeWidth": stroke_width,
-        # "strokeColor": (0, 0, 0),
-        # "hiddenColor": (150, 150, 150),
-        # "width": 1024, "height": 1024,
     }
     exporters.export(obj, str(out_svg), opt=opts)
-
-
-# def svg_to_png(svg_path: Path, png_path: Path, dpi=192):
-#     import cairosvg
-#     cairosvg.svg2png(url=str(svg_path), write_to=str(png_path), dpi=dpi)
 
 
 def main():
@@ -45,13 +31,12 @@ def main():
 
     out_dir = Path(__file__).parent/"render_cache"
     out_dir.mkdir(parents=True, exist_ok=True)
-    base = step_path.stem  #filename without extension
 
     shape = importers.importStep(str(step_path))
 
-    iso_svg   = out_dir / f"{base}_iso.svg"
-    front_svg = out_dir / f"{base}_front.svg"
-    top_svg   = out_dir / f"{base}_top.svg"
+    iso_svg   = out_dir / f"iso.svg"
+    front_svg = out_dir / f"front.svg"
+    top_svg   = out_dir / f"top.svg"
 
     export_svg(shape, iso_svg,   projection_dir=(1, 1, 1))
     export_svg(shape, front_svg, projection_dir=(0, -1, 0))
@@ -61,22 +46,6 @@ def main():
     print(" ", iso_svg)
     print(" ", front_svg)
     print(" ", top_svg)
-
-    # dpi=192
-    # if HAS_CAIROSVG:
-    #     iso_png   = out_dir / f"{base}_iso.png"
-    #     front_png = out_dir / f"{base}_front.png"
-    #     top_png   = out_dir / f"{base}_top.png"
-
-    #     svg_to_png(iso_svg, iso_png, dpi)
-    #     svg_to_png(front_svg, front_png, dpi)
-    #     svg_to_png(top_svg, top_png, dpi)
-
-    #     print(f"Wrote PNGs (via CairoSVG @ {dpi} dpi):")
-    #     print(" ", iso_png)
-    #     print(" ", front_png)
-    #     print(" ", top_png)
-    # else:
 
 if __name__ == "__main__":
     main()
